f8afa09ed06ea1f1d13609530696cfb58f30d0cd
gonczor <wiktor.gonczaronek@gmail.com>
2021-01-12 10:50:35 +0100

    Basic account creation form.

--- src/frontend/urls.py
+++ src/frontend/urls.py
@@ -4,5 +4,6 @@ from django.views.generic import TemplateView
 app_name = 'frontend'
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='index.html'))
+    path('', TemplateView.as_view(template_name='index.html')),
+    path('login/', TemplateView.as_view(template_name='login.html')),
 ]
