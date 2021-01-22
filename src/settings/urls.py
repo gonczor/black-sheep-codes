61cb6b03a77bd716a9cc60d8e5a2ace871865a2e
gonczor <wiktor.gonczaronek@gmail.com>
2021-01-22 21:10:35 +0100

    Admin panel integration

--- src/settings/urls.py
+++ src/settings/urls.py
@@ -13,10 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
     path('', include('frontend.urls')),
+    path('admin/', admin.site.urls),
     path('api/v1/auth/', include('djoser.urls')),
     path('api/v1/auth/', include('djoser.urls.authtoken')),
     path('api/v1/', include('courses.urls')),
