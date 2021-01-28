a7fa87a73665c782c81a62a223e757b29927f83f
gonczor <wiktor.gonczaronek@gmail.com>
2021-01-28 12:21:46 +0100

    Testing utils

---
+++ src/common/tests.py
@@ -0,0 +1,14 @@
+import os
+
+from django.conf import settings
+from django.core.files.uploadedfile import SimpleUploadedFile
+
+
+def get_cover_image() -> SimpleUploadedFile:
+    with open(os.path.join(settings.BASE_DIR, "test_data", "test_image.png"), "rb") as file:
+        content = file.read()
+    return SimpleUploadedFile(
+        name="test_image.jpg",
+        content=content,
+        content_type="image/png"
+    )
