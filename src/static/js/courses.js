7f560e6e2484b6faa49c0c0326173d1634342e5f
gonczor <wiktor.gonczaronek@gmail.com>
2021-01-28 15:48:47 +0100

    Course signup action

--- src/static/js/courses.js
+++ src/static/js/courses.js
@@ -11,8 +11,8 @@ const HelloVueApp = {
             );
             this.courses = response.data.results;
         },
-        async getCourseDetails(x){
-            const courseId = x.target.dataset.id;
+        async getCourseDetails(event){
+            const courseId = event.target.dataset.id;
             const response = await axios.get(
                 '/api/v1/courses/' + courseId +'/',
                 {
@@ -22,12 +22,43 @@ const HelloVueApp = {
                 }
             );
             this.courseDetails = response.data;
+            this.displaySignup = true;
+        },
+        async signUpForCourse() {
+            const courseId = this.courseDetails.id;
+            try {
+                const response = await axios.post(
+                    '/api/v1/course-signups/',
+                    {
+                        course: courseId
+                    },
+                    {
+                        headers: {
+                            Authorization: 'Token ' + window.localStorage.token
+                        },
+                    }
+                );
+                console.log(response);
+            } catch (error) {
+                let message = "";
+                if (error.response.data.nonFieldErrors === undefined){
+                    message = error.response.data.detail
+                } else {
+                     message = error.response.data.nonFieldErrors.join('\n');
+                }
+                alert(message);
+            }
+
+        },
+        hasDetails() {
+            return this.displaySignup;
         }
     },
     data() {
         return {
             courses: [],
-            courseDetails: {}
+            courseDetails: {},
+            displaySignup: false,
         }
     },
     mounted() {
