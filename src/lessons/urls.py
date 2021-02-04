cebfea90a5399aaf037b2a4f14267aa014abb19e
gonczor <wiktor.gonczaronek@gmail.com>
2021-02-04 07:49:58 +0100

    Registered lessons urls

--- src/lessons/urls.py
+++ src/lessons/urls.py
@@ -2,8 +2,10 @@ from rest_framework.routers import SimpleRouter
 
 from lessons.views import LessonViewSet
 
+app_name = "lessons"
+
 router = SimpleRouter()
-router.register("lessons", LessonViewSet, basename="lessons")
+router.register("lessons", LessonViewSet, basename="lesson")
 
 
 urlpatterns = router.urls
