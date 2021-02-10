928275294ab6a2dc321392722342ead8498f0d6a
gonczor <wiktor.gonczaronek@gmail.com>
2021-02-10 22:28:16 +0100

    Listing lessons and sections

--- src/static/js/course_details.js
+++ src/static/js/course_details.js
@@ -34,3 +34,12 @@ app.config.errorHandler = (error, vm, info) => {
     console.log(error);
 };
 app.mount('#app');
+
+/*
+TODO
+Need to set endpoint for listing lessons assigned to a specific course.
+Maybe this should happen on endpoint for courses so that ordering sections and lessons within
+them will be easier. I need only lesson name and ID. Details can be retrieved from the already
+existing endpoint on lessons. Additionally, I need to remember about showing by default the first
+unfinished course and moving on to the next one on clicking "complete".
+ */
