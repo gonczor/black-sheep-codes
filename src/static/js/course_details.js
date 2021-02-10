51e6872da2bec856bff4aa6e00263116ff42167a
gonczor <wiktor.gonczaronek@gmail.com>
2021-02-10 22:24:32 +0100

    Mock up data for sidebar rendering.

--- src/static/js/course_details.js
+++ src/static/js/course_details.js
@@ -2,16 +2,35 @@ const CourseDetailsApp = {
     el: '#app',
     data() {
         return {
-
+            courseId: null,
+            sections: [
+                {
+                    "name": "Introduction",
+                    "id": 123,
+                    "lessons": [
+                        {"name": "Section Intro", "id": 1},
+                        {"name": "Environment Setup", "id": 2}
+                    ]
+                },
+                {
+                    "name": "Basics",
+                    "id": 321,
+                    "lessons": [
+                        {"name": "Running code", "id": 3},
+                        {"name": "Reading user input", "id": 4},
+                        {"name": "Data types", "id": 5}
+                    ]
+                }
+            ]
         }
     },
     mounted() {
-
+        this.courseId = window.location.pathname.split('/').slice(-2)[0];
     }
-}
+};
 
-const app = Vue.createApp(CourseDetailsApp)
+const app = Vue.createApp(CourseDetailsApp);
 app.config.errorHandler = (error, vm, info) => {
     console.log(error);
 };
-app.mount();
+app.mount('#app');
