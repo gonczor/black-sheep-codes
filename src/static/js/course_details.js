9e70c5bd8dea13782ad5515fde214344d824f8ce
gonczor <wiktor.gonczaronek@gmail.com>
2021-02-19 09:51:59 +0100

    Present courses data on course details

--- src/static/js/course_details.js
+++ src/static/js/course_details.js
@@ -1,31 +1,26 @@
 const CourseDetailsApp = {
-    el: '#app',
+    methods: {
+        async fillSectionsList(){
+             const sectionsList = await axios.get(
+                '/api/v1/courses/' + this.courseId +'/retrieve-assigned/',
+                {
+                    headers: {
+                        Authorization: 'Token ' + window.localStorage.token
+                    }
+                }
+            );
+            this.sections = sectionsList.data.sections;
+        }
+    },
     data() {
         return {
             courseId: null,
-            sections: [
-                {
-                    "name": "Introduction",
-                    "id": 123,
-                    "lessons": [
-                        {"name": "Section Intro", "id": 1},
-                        {"name": "Environment Setup", "id": 2}
-                    ]
-                },
-                {
-                    "name": "Basics",
-                    "id": 321,
-                    "lessons": [
-                        {"name": "Running code", "id": 3},
-                        {"name": "Reading user input", "id": 4},
-                        {"name": "Data types", "id": 5}
-                    ]
-                }
-            ]
+            sections: []
         }
     },
     mounted() {
         this.courseId = window.location.pathname.split('/').slice(-2)[0];
+        this.fillSectionsList().then();
     }
 };
 
@@ -37,9 +32,7 @@ app.mount('#app');
 
 /*
 TODO
-Need to set endpoint for listing lessons assigned to a specific course.
-Maybe this should happen on endpoint for courses so that ordering sections and lessons within
-them will be easier. I need only lesson name and ID. Details can be retrieved from the already
+Details can be retrieved from the already
 existing endpoint on lessons. Additionally, I need to remember about showing by default the first
 unfinished course and moving on to the next one on clicking "complete".
  */
