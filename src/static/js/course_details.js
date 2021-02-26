191b895b85edc008ac354724bf3a007186bb259c
gonczor <wiktor.gonczaronek@gmail.com>
2021-02-26 10:18:32 +0100

    Handling quiz on frontend

--- src/static/js/course_details.js
+++ src/static/js/course_details.js
@@ -22,6 +22,26 @@ const CourseDetailsApp = {
                 }
             );
             this.lessonDetails = response.data;
+            if(this.isTest()){
+                this.setupTest()
+            }
+        },
+        setupTest(){
+            this.testQuestionIndex = 0;
+            this.testQuestions = [];
+            if(this.lessonDetails.questions === undefined){
+                return;
+            }
+            this.lessonDetails.questions.forEach((value, index, array) => {
+                this.testQuestions.push({
+                    text: value.text,
+                    answers: value.answers,
+                    markedCorrect: false
+                });
+                if(this.lessonDetails.questions.length > 0){
+                    this.setCurrentTestQuestion();
+                }
+            });
         },
         async markLessonAsComplete(){
             await axios.post(
@@ -60,6 +80,51 @@ const CourseDetailsApp = {
                 return false
             }
             return this.lessonDetails.lessonType.toLowerCase() === "test";
+        },
+        getCorrectAnswersCounter() {
+            let correctAnswers = 0;
+            this.testQuestions.forEach((value, index, array) => {
+                if (value.markedCorrect)
+                    correctAnswers++;
+            });
+            return correctAnswers + '/' + this.testQuestions.length;
+        },
+        checkAnswer(event){
+            const correctAnswers = this.currentTestQuestion.answers.filter((answer) => {
+                return answer.isCorrect
+            });
+            const correctId = correctAnswers.map((answer) => {return answer.id})[0];
+            if (this.selectedAnswerId !== null && Number(this.selectedAnswerId) === correctId){
+                this.currentTestQuestion.markedCorrect = true;
+            } else {
+                this.currentTestQuestion.markedCorrect = false;
+                if(this.selectedAnswerId !== null ){
+                    let answerLabel = document.getElementById('answer-' + this.selectedAnswerId);
+                    answerLabel.classList.add('incorrect-answer');
+                }
+            }
+            let correctAnswerLabel = document.getElementById('answer-' + correctId);
+            correctAnswerLabel.classList.add('correct-answer');
+        },
+        selectAnswer(event){
+            this.selectedAnswerId = event.target.id;
+        },
+        moveToNextQuestion(){
+            if (this.testQuestionIndex < this.testQuestions.length)
+                this.testQuestionIndex++;
+            if(!this.isBeyondLastQuestion())
+                this.setCurrentTestQuestion();
+        },
+        moveToPreviousQuestion() {
+            if(this.testQuestionIndex > 1)
+                this.testQuestionIndex--;
+            this.setCurrentTestQuestion();
+        },
+        setCurrentTestQuestion() {
+            this.currentTestQuestion = this.testQuestions[this.testQuestionIndex];
+        },
+        isBeyondLastQuestion() {
+            return this.testQuestionIndex === this.testQuestions.length;
         }
     },
     data() {
@@ -67,7 +132,11 @@ const CourseDetailsApp = {
             courseId: null,
             sections: [],
             lessonDetails: null,
-            lessonId: null
+            lessonId: null,
+            testQuestionIndex: 0,
+            testQuestions: [],
+            currentTestQuestion: null,
+            selectedAnswerId: null
         }
     },
     mounted() {
