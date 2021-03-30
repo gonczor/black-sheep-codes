d63057177335b24f84a3e02dd7cf5eb5746002d2
gonczor <wiktor.gonczaronek@gmail.com>
2021-03-30 14:57:00 +0200

    Added questions and answers to django admin

--- src/lessons/admin.py
+++ src/lessons/admin.py
@@ -1,12 +1,22 @@
-from django.contrib.admin import ModelAdmin, site
+from django.contrib.admin import ModelAdmin, TabularInline, site
 
-from lessons.models import Exercise, Lesson, Test
+from lessons.models import Answer, Exercise, Lesson, Test, TestQuestion
 
 
 class BaseLessonAdmin(ModelAdmin):
     list_display = ("name", "course_section")
 
 
+class AnswerInline(TabularInline):
+    model = Answer
+    extra = 2
+
+
+class QuestionAdmin(ModelAdmin):
+    inlines = [AnswerInline]
+
+
 site.register(Lesson, BaseLessonAdmin)
 site.register(Exercise, BaseLessonAdmin)
 site.register(Test, BaseLessonAdmin)
+site.register(TestQuestion, QuestionAdmin)
