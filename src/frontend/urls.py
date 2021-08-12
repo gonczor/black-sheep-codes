0fdd8756727a055911c2a6c1dc0c6d2f7428b7f5
gonczor <wiktor.gonczaronek@gmail.com>
2021-08-12 16:46:36 +0200

    Add activation page

--- src/frontend/urls.py
+++ src/frontend/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path
 from django.views.generic import TemplateView
 
+from .views import ActivationView
+
 app_name = "frontend"
 
 urlpatterns = [
     path("", TemplateView.as_view(template_name="index.html")),
+    path("activation/<str:uid>/<str:token>/", ActivationView.as_view()),
     path("gdpr/", TemplateView.as_view(template_name="privacy_policy.html")),
-    path("login/", TemplateView.as_view(template_name="login.html")),
+    path("login/", TemplateView.as_view(template_name="login.html"), name="login"),
     path("courses/", TemplateView.as_view(template_name="courses.html")),
     path("courses/<int:id>/", TemplateView.as_view(template_name="course_details.html")),
 ]
